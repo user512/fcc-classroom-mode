8a7a7c2195e207523d29b0aaa9bd03ff861c7c63
Tom Lee <user512@users.noreply.github.com>
2018-01-29 01:01:25 +0000

    WIP Post Student should return 422

--- server/test/addStudentTest.js
+++ server/test/addStudentTest.js
@@ -1,25 +1,40 @@
-describe addStudent endpoint
-  it should takes student name, email, fCC username, and note and return 200
-  it should takes student name, email, fCC username, and note and save student
-            ===> mongoose.save function gets called with name, email, fcc username, and the note
-  when name is absent
-    it returns 422 and student is not saved
-    it returns jsons name is required
+var addStudentRoute = require('../routes/addStudent');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var express = require('express');
+var app = express();
+var request = require('supertest');
+
+
+describe('POST /add_student', () => {
+  it('should return an error if student name is absent', (done) => {
+    request(app).post('/add_student').expect(422, done);
+  });
+});
+
+
+// describe addStudent endpoint
+//   it should takes student name, email, fCC username, and note and return 200
+//   it should takes student name, email, fCC username, and note and save student
+//             ===> mongoose.save function gets called with name, email, fcc username, and the note
+//   when name is absent
+//     it returns 422 and student is not saved
+//     it returns jsons name is required
     
-  when email is absent
-    it returns 422 and student is not saved
-    it returns jsons email is required
+//   when email is absent
+//     it returns 422 and student is not saved
+//     it returns jsons email is required
     
-  when freeCodeCampUsername is absent
-    it returns 422 and student is not saved
-    it returns jsons freeCodeCampUsername is required
+//   when freeCodeCampUsername is absent
+//     it returns 422 and student is not saved
+//     it returns jsons freeCodeCampUsername is required
     
-  when email is invalid
-    it returns 422 and student is not saved
-    it returns jsons email is invalid
+//   when email is invalid
+//     it returns 422 and student is not saved
+//     it returns jsons email is invalid
     
-  when freeCodeCampUsername is invalid
-    it returns 422 and student is not saved
-    it returns jsons freeCodeCamp is invalid
+//   when freeCodeCampUsername is invalid
+//     it returns 422 and student is not saved
+//     it returns jsons freeCodeCamp is invalid
     
     
